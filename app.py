5f17c97fae6f181cc94b7d0468472932982f6105
manisathangavel <manisa.thangavel@kwalee.com>
2025-04-19 14:45:46 +0530

    next change

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import gradio as gr
 from chatbot_ui import chatbot
+from document_processor import process_documents
 iface = gr.Interface(fn=chatbot, inputs="text", outputs="text", title="Insurance AI Chatbot")
+
 if __name__ == "__main__":
+    process_documents()
     iface.launch(share=False)
