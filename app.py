6f96c0154d33df60408bcc80ae6462a1981a9f56
manisathangavel <manisa.thangavel@kwalee.com>
2025-04-19 13:09:25 +0530

    New commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import gradio as gr
 from chatbot_ui import chatbot
 iface = gr.Interface(fn=chatbot, inputs="text", outputs="text", title="Insurance AI Chatbot")
-iface.launch(share=False)
+if __name__ == "__main__":
+    iface.launch(share=False)
