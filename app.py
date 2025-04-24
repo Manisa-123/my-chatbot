04c4fdc18327ae9f732cc9eb52731f2158f64484
manisathangavel <manisa.thangavel@kwalee.com>
2025-04-24 13:17:59 +0530

    updated

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from document_processor import process_documents
 iface = gr.Interface(fn=chatbot, inputs="text", outputs="text", title="Insurance AI Chatbot")
 
 if __name__ == "__main__":
-    process_documents()
+    # process_documents()
     iface.launch(share=False)
